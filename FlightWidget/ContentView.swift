080dd045629fb98f3fdc120dd8f51c96a1776dec
Vivian <vivnguyen1205@gmail.com>
2024-08-17 14:40:49 +0700

    Initial Commit

---
+++ FlightWidget/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlightWidget
+//
+//  Created by Vivian Nguyen on 2024-08-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello,")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlightWidget/FlightWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightWidgetApp.swift
+//  FlightWidget
+//
+//  Created by Vivian Nguyen on 2024-08-17.
+//
+
+import SwiftUI
+
+@main
+struct FlightWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

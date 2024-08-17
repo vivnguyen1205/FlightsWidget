731ca0f21205a72f4d2ec358fc2921206165fae9
Vivian <vivnguyen1205@gmail.com>
2024-08-17 14:57:32 +0700

    first commit

---
+++ FlightsWidget/FlightsWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  FlightsWidgetBundle.swift
+//  FlightsWidget
+//
+//  Created by Vivian Nguyen on 2024-08-17.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct FlightsWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        FlightsWidget()
+    }
+}
